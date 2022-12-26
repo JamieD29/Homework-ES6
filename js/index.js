e89199c8a6fed0c8004f6d9431ced0457c82015c
Triet Dang <mtriet193@gmail.com>
2022-12-26 16:01:38 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,146 @@
+import Todo from './models/Todo.js';
+
+// let todo1 = new Todo(1, "Test1", false);
+
+// console.log(todo1);
+
+let todoList = [];
+
+let completedList = [];
+
+window.onload = () => {
+    todoList = mapTodoList(JSON.parse(localStorage.getItem('todos')));
+    completedList = mapTodoList(JSON.parse(localStorage.getItem('completedTodo')));
+    renderTodoList(todoListEl, todoList);
+    renderTodoList(completedListEl, completedList);
+}
+
+const todoListEl = document.getElementById('todo');
+
+const completedListEl = document.getElementById('completed');
+
+let addBtn = document.querySelector('#addItem').addEventListener("click", () => {
+    addTodo();
+    localStorage.setItem('todos', JSON.stringify(todoList));
+})
+
+function mapTodoList(local) {
+    let result = new Array();
+    for (let i = 0; i < local.length; i++) {
+        let oldTodo = local[i];
+        let copyTodo = new Todo(
+            oldTodo.id,
+            oldTodo.value
+        );
+
+        result.push(copyTodo);
+    }
+
+    return result;
+}
+
+function addTodo() {
+    const inputTodo = document.querySelector('#newTask').value;
+
+    let id = Math.floor(Math.random() * 1000);
+
+    for (let i = 0; i < todoList.length; i++) {
+        if (id === todoList[i].id) {
+            id = Math.floor(Math.random() * 1000);
+        }
+    }
+
+
+    //Check if the todo is empty
+    const isEmpty = inputTodo === '';
+
+    //Check for duplicate todo 
+    const isDuplicate =
+        todoList.some((todo) => todo.value === inputTodo);
+
+    if (isEmpty) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: "Todo's input is empty",
+        })
+    } else if (isDuplicate) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: "Todo has already added",
+        })
+    }
+    else {
+        let task = new Todo(id, inputTodo);
+        todoList.push(task);
+        renderTodoList(todoListEl, todoList);
+    }
+}
+
+function renderTodoList(interfaceList, list) {
+    let html = "";
+
+    for (let task of list) {
+        html += `
+        <li class="todoEl">
+            ${task.value}
+         <div class="buttons">
+             <button class="" onclick="completeTask(${task.id})">
+             <i class="fas fa-check-circle complete" ></i>
+             </button>  
+             <button class="" onclick="deleteTodo(${task.id})">
+             <i class="fas fa-trash remove" ></i>
+             </button>
+         </div> 
+         </li>
+        `;
+    }
+
+    interfaceList.innerHTML = html;
+}
+
+window.completeTask = (id) => {
+    const list = todoList;
+    let index = list.findIndex((obj) => obj.id === id);
+
+    if (index === -1) return;
+
+    completedList.push(list[index]);
+    list.splice(index, 1);
+
+    localStorage.setItem('todos', JSON.stringify(list));
+    renderTodoList(todoListEl, list);
+
+    localStorage.setItem('completedTodo', JSON.stringify(completedList));
+    renderTodoList(completedListEl, completedList);
+
+}
+
+window.deleteTodo = (id) => {
+    const list = todoList;
+    let index = list.findIndex((obj) => obj.id === id);
+
+    if (index === -1) {
+        index = completedList.findIndex((obj) => obj.id === id);
+        completedList.splice(index, 1);
+        localStorage.setItem('completedTodo', JSON.stringify(completedList));
+        renderTodoList(completedListEl, completedList);
+        return;
+    }
+
+    list.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(list));
+    renderTodoList(todoListEl, list);
+
+}
+
+window.orderBy =  (key,tag) => { //name
+    let order = tag.getAttribute('data-order');
+
+    let arrayResult = _.orderBy(todoList, [key],[order]);
+    renderTodoList(todoListEl, arrayResult); 
+
+    let arrayCompletedResult = _.orderBy(completedList, [key],[order]);
+    renderTodoList(completedListEl, arrayCompletedResult); 
+}
---
+++ js/models/Todo.js
@@ -0,0 +1,9 @@
+class Todo{
+    constructor( id ,value){
+        this.id = id; 
+        this.value = value;
+       
+    }
+}
+
+export default Todo;
